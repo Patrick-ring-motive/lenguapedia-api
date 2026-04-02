ef067d7f42c2a2bca9a2fb0f5f8a9a121ac62942
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:25:35 -0500

    style: js-beautify formatting (index.mjs)

--- index.mjs
+++ index.mjs
@@ -1,32 +1,38 @@
 import fetch from 'node-fetch';
 import http from 'http';
-import { addCorsHeaders } from './modules/cors-headers.mjs';
-import { normalizeRequest, mapResHeaders, applyResponse } from './modules/http-fetch.mjs';
-import { serverRequestResponse } from './server.mjs';
+import {
+  addCorsHeaders
+} from './modules/cors-headers.mjs';
+import {
+  normalizeRequest,
+  mapResHeaders,
+  applyResponse
+} from './modules/http-fetch.mjs';
+import {
+  serverRequestResponse
+} from './server.mjs';
 
-
-process.on('uncaughtException',async e=>console.log(e));
+process.on('uncaughtException', async e => console.log(e));
 
 http.createServer(onRequest).listen(3000);
 
 async function onRequest(req, res) {
-  try{
-  req.socket.setNoDelay();
-  req.socket.setKeepAlive(true);
-  res.socket.setNoDelay();
-  res.socket.setKeepAlive(true);
-  //console.log(res.constructor.toString());
-  let reqDTO = await normalizeRequest(req);
+  try {
+    req.socket.setNoDelay();
+    req.socket.setKeepAlive(true);
+    res.socket.setNoDelay();
+    res.socket.setKeepAlive(true);
+    //console.log(res.constructor.toString());
+    let reqDTO = await normalizeRequest(req);
 
-  let resDTO = await serverRequestResponse(reqDTO);
-  return await applyResponse(res, resDTO);
-  }catch(e){
-    res.statusCode=500;
-    res.end('500 '+e.message);
+    let resDTO = await serverRequestResponse(reqDTO);
+    return await applyResponse(res, resDTO);
+  } catch (e) {
+    res.statusCode = 500;
+    res.end('500 ' + e.message);
   }
 
 }
-
 
 function getType(obj) {
   return obj.constructor.toString().split(' ')[1].split('(')[0];
